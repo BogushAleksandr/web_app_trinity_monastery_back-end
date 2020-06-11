9e53ad63ae8de8d9a7802884b235ab29ef656949
aleksandr <jsashabogush@gmail.com>
2020-06-11 14:31:37 +0300

    add main

---
+++ src/main/java/com/github/aleksandr/bogush/WebAppTrinityMonasteryBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.github.aleksandr.bogush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppTrinityMonasteryBackEndApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppTrinityMonasteryBackEndApplication.class, args);
+    }
+}
